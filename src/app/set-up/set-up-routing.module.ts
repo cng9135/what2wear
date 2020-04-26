e673cfe2663d06a2d1e011eaf87e6d2cb9a1dd03
cng9135 <61843547+cng9135@users.noreply.github.com>
2020-04-26 12:21:16 -0400

    Updated the set up page routing module

---
+++ src/app/set-up/set-up-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SetUpPage } from './set-up.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: SetUpPage
+  }
+];
+
+
+const routes: Routes = [
+  {
+    path: 'SetUp',
+    component: SetUpPage,
+    children: [
+      {
+        path: 'tab1',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          }
+        ]
+      },
+
+      {
+        path: '',
+        redirectTo: '/set-up/SetUp',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/set-up/SetUp',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+
+export class SetUpPageRoutingModule {}
