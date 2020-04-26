03071bde0ef9c7f887ad2de421d80f9c0705417a
cng9135 <61843547+cng9135@users.noreply.github.com>
2020-04-26 11:36:03 -0400

    Update set-up.module.ts

--- src/app/set-up/set-up.module.ts
+++ src/app/set-up/set-up.module.ts
@@ -1,19 +1,18 @@
+import {IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
-import { SetUpPageRoutingModule } from './set-up-routing.module';
-
-import { SetUpPage } from './set-up.page';
+import { SetUpPage} from './set-up.page';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 
 @NgModule({
   imports: [
+    IonicModule,
     CommonModule,
     FormsModule,
-    IonicModule,
-    SetUpPageRoutingModule
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: SetUpPage }])
   ],
   declarations: [SetUpPage]
 })
